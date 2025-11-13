eff0c04e3aea18b128781d78968e987dfe6ee898
daliz-18 <dlizethtrujillo@javeriana.edu.co>
2025-11-13 00:16:58 -0500

    Update UsuarioServiceTest.java

--- scripts/backend/src/test/java/bioprint/ModuloUsuarios/UsuarioServiceTest.java
+++ scripts/backend/src/test/java/bioprint/ModuloUsuarios/UsuarioServiceTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
@@ -128,4 +127,5 @@ void testListarNoModificaListaOriginal() {
     verify(repo).findAll();
 }
 }
+
 
